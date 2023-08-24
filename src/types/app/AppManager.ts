f2c27db184093643d1b6744ff96ddd91b82769c7
AlexandreGdl <guindeuilalexandre@gmail.com>
2023-08-24 17:06:59 +0200

    feat: add solary instance

--- src/types/app/AppManager.ts
+++ src/types/app/AppManager.ts
@@ -1,6 +1,9 @@
 import {Client, GatewayIntentBits, Events, VoiceState} from 'discord.js';
 import {inject, injectable} from 'tsyringe';
 
+const usersToReactTo = ['535943731480756274', '277605153144569866'];
+const userBalls = ["1143939745546776596"];
+const solaryEmoteCode = "1143637205806485564";
 @injectable()
 export class AppManager {
   // client manager
@@ -9,7 +12,7 @@ export class AppManager {
 
   constructor(@inject("PrivateToken") public privateToken?: string) {
     this.appToken = privateToken ?? '';
-    this.client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
+    this.client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions] });
 
     this.init();
   }
@@ -25,6 +28,20 @@ export class AppManager {
   }
 
   private listen() {
+    this.client.on(Events.MessageCreate, (message) => {
+      if (usersToReactTo.includes(message.author.id)) {
+        message.react(solaryEmoteCode)
+        .then(() => message.react('🇸'))
+        .then(() => message.react('🇴'))
+        .then(() => message.react('🇱'))
+        .then(() => message.react('🇦'))
+        .then(() => message.react('🇷'))
+        .then(() => message.react('🇾'))
+        .catch((error) => {
+          console.error('Error reacting to message:', error);
+        });
+      }
+    })
     this.client.on(Events.VoiceStateUpdate, (oldState: VoiceState, newState: VoiceState) => {
       console.log('Voice State Update Triggered');
     });
